59ba43bcaa2d91e2e9d5c1fc1a68c3d1051810e4
ming wu <ming@MacBookPro.lan>
2026-03-01 15:49:29 +1100

    fix

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
   title: 'MING',
   author: 'Ming',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  email: 'matwming114@gmail.com',
+  link: '',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
